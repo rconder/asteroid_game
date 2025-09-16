7928141694b32009ddda45981b8cdef2128f09db
rconder <remzicansinonder@gmail.com>
2025-09-16 23:46:50 +0200

    player shape added

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black" )
+        player.draw(screen)
         pygame.display.flip()
+        dt =  clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
